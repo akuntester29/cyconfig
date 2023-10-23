c6191c57e68d110f7ddc22b56983788180043741
akuntester29 <148743471+akuntester29@users.noreply.github.com>
2023-10-23 12:44:39 +0700

    Update Login.cy.js

--- cypress/e2e/Login.cy.js
+++ cypress/e2e/Login.cy.js
@@ -1,7 +1,7 @@
 describe('E2E Login account', () => {
     it('Login account', () => {
       cy.visit('https://magento.softwaretestingboard.com/');
-      cy.get('.panel > .header > .authorization-link > a').click();
+      cy.get('.pa).click();
       cy.get('#email').click().type('yacobus.ranger@gmail.com');
       cy.get('.login-container > .block-customer-login > .block-content > #login-form > .fieldset > .password > .control > #pass').click().type('Password123');
       cy.get('.login-container > .block-customer-login > .block-content > #login-form > .fieldset > .actions-toolbar > div.primary > #send2').click();
